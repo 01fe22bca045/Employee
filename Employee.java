8e39c1e4ee42a93ee453296c9b895e3d2ca4da3b
EvelynJessica0906 <01fe22bca045@kletech.ac.in>
2024-03-31 21:10:45 +0530

    Extended Employee info code

---
+++ Employee.java
@@ -0,0 +1,8 @@
+class Employee {
+    int id = 001;
+    int sal = 10000;
+
+    void displayDetails() {
+        System.out.println("Id: " + id + "\nSalary: " + sal); // Corrected the space character
+    }
+}
